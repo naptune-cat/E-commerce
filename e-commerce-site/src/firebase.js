3306f4dcea5ed263dfcfbd7482f8f7018ddbfa0d
Yashi Gupta <guptayashi32003@gmail.com>
2024-01-17 23:52:29 +0530

    Basic components added

---
+++ e-commerce-site/src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD89vXCfw9_FWalDf5KUw9NU-o_fT_d9HI",
+  authDomain: "e-commerce-25bc7.firebaseapp.com",
+  projectId: "e-commerce-25bc7",
+  storageBucket: "e-commerce-25bc7.appspot.com",
+  messagingSenderId: "550980283374",
+  appId: "1:550980283374:web:7f93169c9b7f1c61257cff",
+  measurementId: "G-Q557V2ZWZP"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
